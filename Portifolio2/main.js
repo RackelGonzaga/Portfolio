15f7465b34220bda45218dd1765dd43653c8ff7b
RackelGonzaga <gonzagarackel@gmail.com>
2025-09-05 12:10:55 -0300

    Update main.js
    add hidden

--- Portifolio2/main.js
+++ Portifolio2/main.js
@@ -23,6 +23,19 @@ function showOnScroll() {
     }
   });
 }
+//"hidden"
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      entry.target.classList.remove('hidden');
+    }
+  });
+});
+
+//"hidden"
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => observer.observe(el));
 
 window.onload = () => {
   typeWriter();
